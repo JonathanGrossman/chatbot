0f218acc3b0de6f2211122a520178bd64712f198
JonathanGrossman <jon@jonegrossman.com>
2019-12-31 14:50:58 -0600

    added startswith and endswith functions -- just the basics to get it working

--- boto.py
+++ boto.py
@@ -1,8 +1,28 @@
-"""
-This is the template server side for ChatBot
-"""
 from bottle import request, route, run, static_file, template
 import json
+
+
+def check_starts_with(user_input):
+    if user_input.startswith("Hello"):
+        return "Hello. "
+    elif user_input.startswith("Hey"):
+        return "Hey is for horses. "
+    else:
+        return ""
+
+def check_ends_with(user_input):
+    if user_input.endswith("?"):
+        response = "Great question."
+        return response
+    elif user_input.endswith("!"):
+        response = "I'm excited too."
+        return response
+    elif user_input.endswith("."):
+        response = "OK. Let me respond."
+        return response
+    else:
+        return ""
+
 
 @route('/', method='GET')
 def index():
@@ -12,7 +32,8 @@ def index():
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
-    return json.dumps({"animation": "inlove", "msg": user_message})
+    boto_response = check_starts_with(user_message) + check_ends_with(user_message)
+    return json.dumps({"animation": "inlove", "msg": boto_response})
 
 
 @route("/test", method='POST')
