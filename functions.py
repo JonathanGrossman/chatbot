8d3f1adec8bcf136709d1abb22b4650d7589c045
JonathanGrossman <jon@jonegrossman.com>
2020-01-04 06:34:48 -0600

    added functions to ask where from and favorite foods

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ def check_for_cursing(message):
             return "Please, no cursing. Try again. This time, keep it classy. Type boto for more information."
         else:
             return check_type(message)
+            # return check_type(message) + " Type 'boto' for more information."
 
 
 def check_type(message):
@@ -57,27 +58,73 @@ def check_ends_with(user_input):
 
 
 def additional_processing(message):
-    array = message.split()
-    new = array[1:]
-    joined = " ".join(new)
-    if message.lower().startswith("where"):
-        return "where"
-    elif message.lower().startswith("robber's language"):
-        new = array[2:]
-        joined = " ".join(new)
-        return translate_to_robbers(joined)
-    elif message.lower().startswith("reverse"):
-        return reverse_message(joined)
-    elif message.lower().startswith("palindrome"):
-        return palindrome(joined)
+    if check_for_location(message) is not None:
+        return check_for_location(message)
+    if check_for_food(message) is not None:
+        return check_for_food(message)
+    if check_for_translate_to_robbers(message) is not None:
+        return check_for_translate_to_robbers(message)
+    if check_for_reverse(message) is not None:
+        return check_for_reverse(message)
+    if check_for_palindrome(message) is not None:
+        return check_for_palindrome(message)
+    return counter_number_characters(message)
+
+
+def counter_number_characters(message):
+    counter = 0
+    for item in message:
+        counter += 1
+    if counter == 1:
+        return check_vowel(message)
     else:
-        counter = 0
-        for item in message:
-            counter += 1
-        if counter == 1:
-            return check_vowel(message)
-        else:
-            return f"Your message is {counter} characters."
+        return f"I love your name! I'm a robot, so I'm great with names. Yours is {message}. As a side note, " \
+               f" your name is {counter} letters. Are you from Kansas City, Chicago, New York, or Tel Aviv? If so, " \
+               f"type the city's name. Otherwise, type, 'nope'."
+
+
+def check_for_location(message):
+    locations = ["tel aviv", "kansas city", "chicago", "new york", "nope"]
+    for place in locations:
+        if message.lower().find(place) != -1:
+            return respond_location(place)
+
+
+def respond_location(city):
+    next_prompt = "Let's see if we have anything in common. Type the names of the following foods that you like. " \
+                     "Peanut butter, chocolate, and sweet potato. If you like none, type 'none'?"
+    if city == "tel aviv":
+        return "Really?! Great city. " + next_prompt
+    elif city == "kansas city":
+        return "No way! I grew up there. " + next_prompt
+    elif city == "chicago":
+        return "The windy city. I lived there for a long time. " + next_prompt
+    elif city == "new york":
+        return "There's no other place like NYC. " + next_prompt
+    else:
+        return "Shoot. " + next_prompt
+
+
+def check_for_food(message):
+    foods = ["peanut butter", "chocolate", "sweet potato", "none"]
+    for item in foods:
+        if message.lower().find(item) != -1:
+            return respond_food(message)
+
+
+def respond_food(message):
+    foods = ["peanut butter", "chocolate", "sweet potato"]
+    array = message.split(", ")
+    foods_in_common = []
+    for item in foods:
+        for entry in array:
+            if item == entry.lower():
+                foods_in_common.append(item)
+    string = ", ".join(foods_in_common)
+    if string == "":
+        return "Shoot. I guess we don't have that in common. Type 'boto' to learn what I can do."
+    else:
+        return f"Alright! I also like {string}. Type 'boto' to learn more about what I can do."
 
 
 def return_max_of_two(numbers):
@@ -105,6 +152,14 @@ def return_max_of_three(numbers):
         return "You entered the same number three times!"
 
 
+def check_for_translate_to_robbers(message):
+    array = message.split()
+    if message.lower().startswith("robber's language"):
+        new = array[2:]
+        joined = " ".join(new)
+        return translate_to_robbers(joined)
+
+
 def translate_to_robbers(message):
     translated = ""
     if message == "":
@@ -118,6 +173,14 @@ def translate_to_robbers(message):
         return translated
 
 
+def check_for_reverse(message):
+    array = message.split()
+    new = array[1:]
+    joined = " ".join(new)
+    if message.lower().startswith("reverse"):
+        return reverse_message(joined)
+
+
 def reverse_message(message):
     if message == "":
         return "You forgot to enter what you wanted me to reverse."
@@ -126,6 +189,15 @@ def reverse_message(message):
         for l in message:
             reversed_message = l + reversed_message
         return reversed_message
+
+
+def check_for_palindrome(message):
+    print(message)
+    array = message.split()
+    new = array[1:]
+    joined = " ".join(new)
+    if message.lower().startswith("palindrome"):
+        return palindrome(joined)
 
 
 def palindrome(message):
